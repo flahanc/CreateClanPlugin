30697ca0d2e0a99839a61e3aa8c121e6bf548419
flahanc <mivoder@gmail.com>
2025-01-18 17:44:43 +0300

    Update CallTimeCounter.plugin.js

--- CallTimeCounter.plugin.js
+++ CallTimeCounter.plugin.js
@@ -7,19 +7,36 @@
  * @version 1.0.0
  */
 
-(function() {
-    const addButton = document.querySelector('.add-friend-button'); // Измените селектор на правильный
-
-    if (addButton) {
-        const clanButton = document.createElement('button');
-        clanButton.innerText = 'Добавить в клан';
-        clanButton.className = 'clan-button'; // Добавьте стиль по желанию
-
-        addButton.parentNode.insertBefore(clanButton, addButton.nextSibling);
-
-        clanButton.addEventListener('click', function() {
-            // Логика для добавления в клан
-            alert('Клан добавлен!');
-        });
+module.exports = class CreateClanPlugin {
+    start() {
+        this.addClanButton();
     }
-})();
+
+    stop() {
+        this.removeClanButton();
+    }
+
+    addClanButton() {
+        const addButton = document.querySelector('.add-friend-button'); // Измените селектор на правильный
+
+        if (addButton) {
+            const clanButton = document.createElement('button');
+            clanButton.innerText = 'Добавить в клан';
+            clanButton.className = 'clan-button'; // Добавьте стиль по желанию
+
+            addButton.parentNode.insertBefore(clanButton, addButton.nextSibling);
+
+            clanButton.addEventListener('click', () => {
+                // Логика для добавления в клан
+                alert('Клан добавлен!');
+            });
+        }
+    }
+
+    removeClanButton() {
+        const clanButton = document.querySelector('.clan-button');
+        if (clanButton) {
+            clanButton.remove();
+        }
+    }
+};
